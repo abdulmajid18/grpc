e6166738b73c1f512bf6c170dcf69523c53d0594
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-02 12:33:12 +0000

    new

--- productinfo/service/main.go
+++ productinfo/service/main.go
@@ -7,7 +7,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	pb "github.com/grpc/productinfo/service/ecommerce"
+	pb "github.com/abdulmajid18/grpc/productinfo/service/ecommerce"
 )
 
 // server is used to implement ecommmerce/product_info
@@ -16,11 +16,16 @@ type server struct {
 	productMap map[string]pb.Product
 }
 
-// AddProduct impleentts ecommerce.AddProduct
+// AddProduct implements ecommerce.AddProduct
 
-func (s *server)AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
 	out, err := uuid.NewV4()
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "Error while generaating Product ID", error)
-		
 	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]pb.Product)
+	}
+
+}
